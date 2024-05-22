efda58adcd54734d0301b95b17d7a4de1a985afb
User <yunusovhumoyun13@gmail.com>
2024-05-22 19:57:57 +0500

    Data

---
+++ app/src/main/java/com/example/olim_prototip/data/model/SubjectData.kt
@@ -0,0 +1,11 @@
+package com.example.olim_prototip.data.model
+
+import androidx.annotation.DrawableRes
+
+data class SubjectData(
+    val id: Int,
+    val name: String,
+    @DrawableRes
+    val image: Int,
+    val description: String
+)
