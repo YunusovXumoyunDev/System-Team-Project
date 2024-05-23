8e7a7da8a2c25d10dbf2e9084da839881ee56064
carlettw <ravshansmile@gmail.com>
2024-05-23 07:25:57 +0500

    kirish.html uchun javascript qismi script.js fayliga yozildi

--- web site/script.js
+++ web site/script.js
@@ -16,3 +16,22 @@ userBtn.addEventListener('mouseenter', () => {
 userBtn.addEventListener('mouseleave', () => {
   userBtn.style.transform = 'scale(1)';
 });
+
+// kirish.html fali uchun
+
+const loginBtn = document.querySelector('.login-btn-kirish');
+const registerBtn = document.querySelector('.register-btn-kirish');
+
+loginBtn.addEventListener('click', () => {
+  const usernameInput = document.querySelector('.user-box-kirish input[name="username"]');
+  const passwordInput = document.querySelector('.user-box-kirish input[name="password"]');
+
+  // Kirish logikasi
+  console.log('Username:', usernameInput.value);
+  console.log('Password:', passwordInput.value);
+});
+
+registerBtn.addEventListener('click', () => {
+  // Ro'yhatdan o'tish logikasi
+  window.location.href = 'register.html';
+});
